d1d744c8dcbd8d7b7492a883736286e8a1b89cf8
Michael Correa <98114967+scnc-tch@users.noreply.github.com>
2022-03-29 21:20:41 -0400

    3.29.22

--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+let btn = document.getElementById("btn")
+let color = document.querySelector(".color")
+
+btn.addEventListener("click", function(){
+    let newColor = '#'
+    for(let i = 0; i < 6; i++){
+        newColor += hex[getRandom()]
+    }
+    document.body.style.backgroundColor = newColor
+    color.textContent = newColor
+})
+
+function getRandom(){
+    return Math.floor(Math.random() * hex.length)
+}
